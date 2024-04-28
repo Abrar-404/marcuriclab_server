f879af617b871b9be2c1f8fa821df2b1df81c252
Al Fattaul Islam <139103740+Abrar-404@users.noreply.github.com>
2024-04-28 16:45:55 +0600

    made new collection

--- index.js
+++ index.js
@@ -44,12 +44,17 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-    const featureCardCollection = client
-      .db('xenricDB')
-      .collection('featureCards');
+    const userCollection = client
+      .db('marcuriclab')
+      .collection('userData');
 
 
-
+   app.post('/userData', async (req, res) => {
+     const userAdd = req.body;
+     console.log(userAdd);
+     const result = await userCollection.insertOne(userAdd);
+     res.send(result);
+   });
 
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 });
